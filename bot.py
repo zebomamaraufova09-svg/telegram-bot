3ddbf9d44498877f1860330e9c907aa75fce5ab6
zebomamaraufova09-svg <zebomamaraufova09@gmail.com>
2026-02-22 08:32:28 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,16 +5,32 @@ from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTyp
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
 
-async def forward_to_admin(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.message:
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    message = update.message
+    user_id = message.from_user.id
+
+    # 🔹 Agar ADMIN reply qilayotgan bo‘lsa
+    if user_id == ADMIN_ID and message.reply_to_message:
+        original_user = message.reply_to_message.forward_from
+        if original_user:
+            await context.bot.send_message(
+                chat_id=original_user.id,
+                text=message.text
+            )
+        return
+
+    # 🔹 Agar oddiy user yozgan bo‘lsa
+    if user_id != ADMIN_ID:
         await context.bot.forward_message(
             chat_id=ADMIN_ID,
-            from_chat_id=update.message.chat_id,
-            message_id=update.message.message_id
+            from_chat_id=message.chat.id,
+            message_id=message.message_id
         )
-        await update.message.reply_text("Xabaringiz qabul qilindi 🤍")
+
+        await message.reply_text("Xabaringiz qabul qilindi 🤍")
+
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, forward_to_admin))
-
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 app.run_polling()
