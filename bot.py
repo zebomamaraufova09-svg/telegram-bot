132af88ce64889cb33243fb4be0a8b43f5716ced
zebomamaraufova09-svg <zebomamaraufova09@gmail.com>
2026-02-22 08:38:20 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,21 +6,23 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
     message = update.message
     user_id = message.from_user.id
 
-    # 🔹 Agar ADMIN reply qilayotgan bo‘lsa
+    # Agar admin reply qilayotgan bo‘lsa
     if user_id == ADMIN_ID and message.reply_to_message:
-        original_user = message.reply_to_message.forward_from
-        if original_user:
+        original = message.reply_to_message
+
+        # Forward qilingan xabarni aniqlaymiz
+        if original.forward_from:
+            target_user_id = original.forward_from.id
             await context.bot.send_message(
-                chat_id=original_user.id,
+                chat_id=target_user_id,
                 text=message.text
             )
         return
 
-    # 🔹 Agar oddiy user yozgan bo‘lsa
+    # Agar oddiy user yozgan bo‘lsa
     if user_id != ADMIN_ID:
         await context.bot.forward_message(
             chat_id=ADMIN_ID,
@@ -30,7 +32,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
         await message.reply_text("Xabaringiz qabul qilindi 🤍")
 
-
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+
 app.run_polling()
